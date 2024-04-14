a2bb652dd8f1bd76d5b47be1fbd6b6817290dbe8
Mad Mike <anikinmihail34@gmail.com>
2024-04-14 16:23:55 +0300

    Road logic complete

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -8,17 +8,42 @@ import GrassTile from "./GrassTile"
 import { Fragment, useEffect, useRef, useState } from "react"
 import { Button } from "./ui/button"
 import GrownTree from "./GrownTree"
+import PlantedTree from "./PlantedTree"
+import MediumTree from "./MediumTree"
+import RoadTile from "./RoadTile"
 
-export type BlockType = "grass-1" | "grass-2" | "plane" | "null"
-export type ObjectType = "tree" | "null"
+export type BlockType =
+    | "grass-1"
+    | "grass-2"
+    | "plane"
+    | "null"
+    | "road"
+    | "road-t"
+    | "road-r"
+    | "road-l"
+    | "road-b"
+    | "road-tr"
+    | "road-y"
+    | "road-tl"
+    | "road-rb"
+    | "road-x"
+    | "road-bl"
+    | "road-trb"
+    | "road-tbl"
+    | "road-trl"
+    | "road-rbl"
+    | "road-c"
+export type ObjectType = "tree" | "planted-tree" | "medium-tree" | "null"
 export type Action =
     | "Clear from grass"
     | "Grow a tree"
     | "Chop down a tree"
     | "Plant grass"
+    | "Water a tree"
+    | "Build a road"
 export type ObjectID = { x: number; y: number }
 
-type generateGrassTileProps = {
+type generateTileProps = {
     props: BlockInfo
     isInteractive: boolean
     action: Action | null
@@ -47,7 +72,7 @@ function generateGrassTile({
     action,
     selectedObjectID,
     handleClick,
-}: generateGrassTileProps) {
+}: generateTileProps) {
     if (props && props.id) {
         return (
             <GrassTile
@@ -63,14 +88,51 @@ function generateGrassTile({
     return <></>
 }
 
-function generateTree({ props }: generateTreeProps) {
-    if (props.id) {
+function generateRoadTile({
+    props,
+    isInteractive,
+    action,
+    selectedObjectID,
+    handleClick,
+}: generateTileProps) {
+    if (props && props.id) {
         return (
-            <GrownTree
-                position={[props.id.x * 2, 1, props.id.y * 2]}
-                rotation={[0, props.rotation, 0]}
+            <RoadTile
+                info={props}
+                props={{ position: [props.id.x * 2, 0, props.id.y * 2] }}
+                isInteractive={isInteractive}
+                action={action}
+                selectedObjectID={selectedObjectID}
+                handleClick={handleClick}
             />
         )
+    }
+    return <></>
+}
+
+function generateTree({ props }: generateTreeProps) {
+    if (props.id) {
+        if (props.type === "tree")
+            return (
+                <GrownTree
+                    position={[props.id.x * 2, 1, props.id.y * 2]}
+                    rotation={[0, props.rotation, 0]}
+                />
+            )
+        else if (props.type === "planted-tree")
+            return (
+                <PlantedTree
+                    position={[props.id.x * 2, 1, props.id.y * 2]}
+                    rotation={[0, props.rotation, 0]}
+                />
+            )
+        else if (props.type === "medium-tree")
+            return (
+                <MediumTree
+                    position={[props.id.x * 2, 1, props.id.y * 2]}
+                    rotation={[0, props.rotation, 0]}
+                />
+            )
     }
     return <></>
 }
@@ -85,6 +147,76 @@ export default function Scene() {
         null,
     )
     const [action, setAction] = useState<Action | null>(null)
+
+    //Game functions
+    function handleRoadCheck() {
+        let map = blockMap
+        if (map !== null) {
+            for (let i = 0; i < map.length; i++) {
+                for (let j = 0; j < map.length; j++) {
+                    if (map[i][j].type.includes("road")) {
+                        let top = 0
+                        let bottom = 0
+                        let left = 0
+                        let right = 0
+                        if (
+                            map[i - 1] !== undefined &&
+                            map[i - 1][j].type.includes("road")
+                        ) {
+                            top = 1
+                        }
+                        if (
+                            map[i][j + 1] !== undefined &&
+                            map[i][j + 1].type.includes("road")
+                        ) {
+                            right = 1
+                        }
+                        if (
+                            map[i + 1] !== undefined &&
+                            map[i + 1][j].type.includes("road")
+                        ) {
+                            bottom = 1
+                        }
+                        if (
+                            map[i][j - 1] !== undefined &&
+                            map[i][j - 1].type.includes("road")
+                        ) {
+                            left = 1
+                        }
+
+                        //top right bottom left
+                        const combinations = {
+                            "0000": "road",
+                            "0001": "road-r",
+                            "0010": "road-b",
+                            "0011": "road-br",
+                            "0100": "road-l",
+                            "0101": "road-x",
+                            "0110": "road-bl",
+                            "0111": "road-rbl",
+                            "1000": "road-t",
+                            "1001": "road-tr",
+                            "1010": "road-y",
+                            "1011": "road-trb",
+                            "1100": "road-tl",
+                            "1101": "road-trl",
+                            "1110": "road-tbl",
+                            "1111": "road-c",
+                        }
+                        const combination = `${top}${right}${bottom}${left}`
+
+                        if (combination in combinations) {
+                            //@ts-expect-error
+                            map[i][j].type = combinations[combination]
+                        }
+                        console.log(map[i][j].type)
+                    }
+                }
+            }
+        }
+        setBlockMap(map)
+        localStorage.setItem("blockMap", JSON.stringify(map))
+    }
 
     //Initial state
     //! Currently using local storage -> change to server in near future
@@ -177,7 +309,8 @@ export default function Scene() {
                     else if (randomN < 50) rotation = Math.PI
                     else if (randomN < 75) rotation = (3 * Math.PI) / 2
 
-                    objMap[selectedObjectID.x][selectedObjectID.y].type = "tree"
+                    objMap[selectedObjectID.x][selectedObjectID.y].type =
+                        "planted-tree"
                     objMap[selectedObjectID.x][selectedObjectID.y].rotation =
                         rotation
                 }
@@ -218,6 +351,43 @@ export default function Scene() {
                     setBlockMap(map)
                     localStorage.setItem("blockMap", JSON.stringify(map))
                 }
+            } else if (action === "Water a tree") {
+                if (
+                    objMap[selectedObjectID.x][selectedObjectID.y].type !==
+                    "null"
+                ) {
+                    if (
+                        objMap[selectedObjectID.x][selectedObjectID.y].type ===
+                        "planted-tree"
+                    ) {
+                        objMap[selectedObjectID.x][selectedObjectID.y].type =
+                            "medium-tree"
+                    } else if (
+                        objMap[selectedObjectID.x][selectedObjectID.y].type ===
+                        "medium-tree"
+                    ) {
+                        objMap[selectedObjectID.x][selectedObjectID.y].type =
+                            "tree"
+                    }
+                    setObjectMap(objMap)
+                    localStorage.setItem("objectMap", JSON.stringify(objMap))
+                }
+            } else if (action === "Build a road") {
+                if (
+                    objMap[selectedObjectID.x][selectedObjectID.y].type ===
+                    "null"
+                ) {
+                    //Randomly setting type of grass and block's rotation
+                    const type: BlockType = "road"
+                    const rotation = 0
+
+                    map[selectedObjectID.x][selectedObjectID.y].type = type
+                    map[selectedObjectID.x][selectedObjectID.y].rotation =
+                        rotation
+                    setBlockMap(map)
+                    localStorage.setItem("blockMap", JSON.stringify(map))
+                }
+                handleRoadCheck()
             }
         }
         setAction(null)
@@ -264,7 +434,15 @@ export default function Scene() {
                         if (isInteractive) setAction("Grow a tree")
                     }}
                 >
-                    Вырастить дерево
+                    Посадить дерево
+                </Button>
+                <Button
+                    className="bg-teal-500"
+                    onClick={() => {
+                        if (isInteractive) setAction("Water a tree")
+                    }}
+                >
+                    Полить дерево
                 </Button>
                 <Button
                     className="bg-rose-600"
@@ -274,6 +452,14 @@ export default function Scene() {
                 >
                     Срубить дерево
                 </Button>
+                <Button
+                    className="bg-black"
+                    onClick={() => {
+                        if (isInteractive) setAction("Build a road")
+                    }}
+                >
+                    Построить дорогу
+                </Button>
             </div>
             <Canvas camera={{ position: cameraPosition, fov, far: 15000 }}>
                 <OrbitControls
@@ -281,6 +467,7 @@ export default function Scene() {
                     enableDamping={false}
                     ref={orbitControlsRef}
                     //!Controls boundaries, enable on production
+                    /*
                     maxDistance={200}
                     minDistance={100}
                     maxAzimuthAngle={Math.PI * 0.75}
@@ -295,7 +482,7 @@ export default function Scene() {
                             } else if (currentY < 50) {
                                 e?.target.object.position.setY(50)
                             }
-                    }}
+                    }}*/
                 />
                 <directionalLight position={[2, 4, 1]} />
                 <ambientLight intensity={0.1} />
@@ -304,33 +491,70 @@ export default function Scene() {
                     <>
                         {blockMap.map((row, i) => (
                             <Fragment key={i}>
-                                {row.map((obj, j) => (
-                                    <Fragment key={j}>
-                                        {generateGrassTile({
-                                            props: obj,
-                                            isInteractive,
-                                            action,
-                                            selectedObjectID,
-                                            handleClick: () => {
-                                                if (isInteractive) {
-                                                    orbitControlsRef.current.saveState()
-                                                    if (
-                                                        selectedObjectID !==
-                                                        obj.id
-                                                    ) {
-                                                        setSelectedObjectID(
-                                                            obj.id,
-                                                        )
-                                                    } else {
-                                                        setSelectedObjectID(
-                                                            null,
-                                                        )
-                                                    }
-                                                }
-                                            },
-                                        })}
-                                    </Fragment>
-                                ))}
+                                {row.map((obj, j) => {
+                                    if (
+                                        obj.type === "grass-1" ||
+                                        obj.type === "grass-2" ||
+                                        obj.type === "plane"
+                                    )
+                                        return (
+                                            <Fragment key={j}>
+                                                {generateGrassTile({
+                                                    props: obj,
+                                                    isInteractive,
+                                                    action,
+                                                    selectedObjectID,
+                                                    handleClick: () => {
+                                                        if (isInteractive) {
+                                                            orbitControlsRef.current.saveState()
+                                                            if (
+                                                                selectedObjectID !==
+                                                                obj.id
+                                                            ) {
+                                                                setSelectedObjectID(
+                                                                    obj.id,
+                                                                )
+                                                            } else {
+                                                                setSelectedObjectID(
+                                                                    null,
+                                                                )
+                                                            }
+                                                        }
+                                                    },
+                                                })}
+                                            </Fragment>
+                                        )
+                                    else if (obj.type.includes("road")) {
+                                        obj.rotation = 0
+                                        return (
+                                            <Fragment key={j}>
+                                                {generateRoadTile({
+                                                    props: obj,
+                                                    isInteractive,
+                                                    action,
+                                                    selectedObjectID,
+                                                    handleClick: () => {
+                                                        if (isInteractive) {
+                                                            orbitControlsRef.current.saveState()
+                                                            if (
+                                                                selectedObjectID !==
+                                                                obj.id
+                                                            ) {
+                                                                setSelectedObjectID(
+                                                                    obj.id,
+                                                                )
+                                                            } else {
+                                                                setSelectedObjectID(
+                                                                    null,
+                                                                )
+                                                            }
+                                                        }
+                                                    },
+                                                })}
+                                            </Fragment>
+                                        )
+                                    }
+                                })}
                             </Fragment>
                         ))}
                     </>
@@ -343,7 +567,11 @@ export default function Scene() {
                             <Fragment key={i}>
                                 {row.map((obj, j) => {
                                     const type = obj.type
-                                    if (type === "tree") {
+                                    if (
+                                        type === "tree" ||
+                                        type === "medium-tree" ||
+                                        type === "planted-tree"
+                                    ) {
                                         return (
                                             <Fragment key={j}>
                                                 {generateTree({
